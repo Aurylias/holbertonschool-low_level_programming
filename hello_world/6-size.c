b07ac606d74861575dd1ccc19017b6e1f9e5cbaf
Aurylias <brycevermorel@gmail.com>
2026-06-22 12:02:50 +0200

    Change typo in 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - Entry Point
 * printf: Output different size of multiple type
@@ -10,7 +11,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of an long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of an long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of an long long int: %d byte(s)", sizeof(long long int));
 	printf("Size of a float: %d byte(s)", sizeof(float));
 	return (0);
 }
