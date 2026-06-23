9f7501a0275107c2ae7e9664076d7eaaa2dc8da8
Aurylias <brycevermorel@gmail.com>
2026-06-23 11:10:26 +0200

    Fixed betty typo and changed comment in 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Print the alphabet
+ * main - Print the base16 symboles
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int number;
-  char letter;
+	char letter;
 
 	for (number = 48; number < 58; number++)
-  {
-    putchar(number);
-  }
+	{
+		putchar(number);
+	}
 
-  for (letter = 97; letter < 103; letter++)
-  {
-    putchar(letter);
-  }
+	for (letter = 97; letter < 103; letter++)
+	{
+		putchar(letter);
+	}
 
 	putchar(10);
 
