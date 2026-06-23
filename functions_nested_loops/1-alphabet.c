10755c6041017369911b68e5f5d03a0115d382fe
Aurylias <brycevermorel@gmail.com>
2026-06-23 14:42:37 +0200

    Fixed betty typo and missing ; in 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,24 +1,29 @@
 #include <main.h>
 
 /**
- * main - A function which print the alphabet
- * Return: Always 0 (Success)
+ * print_alphabet - Print the alphabet
+ * Return: None
  */
 
- void print_alphabet(void)
- {
+void print_alphabet(void)
+{
 	char letter;
 
-	for(letter = 'a'; letter <= 'z', letter++)
+	for (letter = 'a'; letter <= 'z', letter++)
 	{
 		_putchar(letter);
 	}
 
 	_putchar('\n')
- }
+}
 
- int main(void)
- {
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
 	print_alphabet();
-	return (O)
- }
+	return (O);
+}
