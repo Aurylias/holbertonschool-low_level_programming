d3c33fc6cf9e24f29418acf97eb5ff6da56d777e
Aurylias <brycevermorel@gmail.com>
2026-06-23 16:47:22 +0200

    Testing

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -13,11 +13,13 @@ int print_last_digit(int n)
 	if (last < 0)
 	{
 		_putchar(-last + '0');
+		return (-last);
 	}
 	else
 	{
 		_putchar(last + '0');
+		return (last);
 	}
 
-	return (last);
+	
 }
