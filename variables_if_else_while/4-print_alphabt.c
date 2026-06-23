315872bee10cbd2be776875af8a4beb2428c2038
Aurylias <brycevermorel@gmail.com>
2026-06-23 09:49:10 +0200

    Add 4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		if (a == 113 || a == 101)
+		{
+
+		}
+		else
+		{
+			putchar(a);
+		}
+	}
+
+	putchar(10);
+
+	return (0);
+}
