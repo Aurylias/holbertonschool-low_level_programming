5845fb55d53280cb6a64cd75d8251d2bf42b909a
Aurylias <brycevermorel@gmail.com>
2026-06-23 15:16:07 +0200

    Add 3 and change main.h

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _islower - Print the alphabet ten times
+ *@c: The char value
+ * Return: None
+ */
+
+int _islower(int c)
+{
+	int letter;
+	int check = 0;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (c != letter)
+		{
+			check++;
+		}
+	}
+
+	if (check > 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
