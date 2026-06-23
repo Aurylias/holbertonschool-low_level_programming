e0ecc9d9125082040e71da0831dc7b43724e974e
Aurylias <brycevermorel@gmail.com>
2026-06-23 16:18:42 +0200

    Add 6 and change main.h

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Return the absolute value of an integer
+ *@n: The value of an int
+ * Return: None
+ */
+
+ int _abs(int n)
+ {
+	if (n < 0)
+	{
+		return (-n)
+	}
+	else
+	{
+		return (n);
+	}
+ }
