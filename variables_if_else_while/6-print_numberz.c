3246b8f7c151c0e13b70e7900c1e90f869bff588
Aurylias <brycevermorel@gmail.com>
2026-06-23 10:47:11 +0200

    Add 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+	int char_value = 48;
+
+	do {
+		putchar(char_value);
+		a++;
+		char_value++;
+	} while (a < 10);
+	printf("\n");
+
+	return (0);
+}
