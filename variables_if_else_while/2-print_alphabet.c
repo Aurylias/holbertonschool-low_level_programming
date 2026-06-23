9ea6f65b4fb2a79a7d28809f797ce0b80ef1e433
Aurylias <brycevermorel@gmail.com>
2026-06-23 09:37:43 +0200

    Add 2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}
