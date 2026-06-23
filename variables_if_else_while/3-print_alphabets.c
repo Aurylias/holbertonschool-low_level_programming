bd325cd40161c59c1295856b138650b309f68d0a
Aurylias <brycevermorel@gmail.com>
2026-06-23 09:45:07 +0200

    Add 3-print_alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+
+	for (a = 65; a <= 90; a++)
+	{
+		putchar(a);
+	}
+
+	putchar(10);
+
+	return (0);
+}
