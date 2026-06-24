7da3fe11c828fa478e8bea78ef7b4ec21c1ba88a
Aurylias <brycevermorel@gmail.com>
2026-06-24 15:13:32 +0200

    Add 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Return a * b
+ * @a: One of the value to multiple
+ * @b: One of the value to multiple
+ * Return: An int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
