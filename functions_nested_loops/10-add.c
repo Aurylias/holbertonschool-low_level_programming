5bbe85bbb89250b06eb532bd350d410a8baabc17
Aurylias <brycevermorel@gmail.com>
2026-06-24 10:51:47 +0200

    Fixed betty typo 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,10 +2,12 @@
 
 /**
  * add - Return result of a + b
+ * @a = a value
+ * @b = b value
  * Return: A int
  */
 
- int add(int a, int b)
- {
-	return a + b;
- }
+int add(int a, int b)
+{
+	return (a + b);
+}
