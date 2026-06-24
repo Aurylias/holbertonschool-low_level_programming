884dc8fbeffbf8f8be943edcedc8c9b7d2dca092
Aurylias <brycevermorel@gmail.com>
2026-06-24 09:53:03 +0200

    Testing

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,21 +11,29 @@ void jack_bauer(void)
 	int hours_digit;
 	int minutes_tens_digit;
 	int minute_digit;
+	int hours_digit_turn = 0;
 
 	for (hours_tens_digit = 0; hours_tens_digit <= 2; hours_tens_digit++)
 	{
-		for (hours_digit = 0; hours_digit <= 9; hours_digit++)
+		for (hours_digit = 0; hours_digit_turn <= 2; hours_digit++)
 		{
 			for (minutes_tens_digit = 0; minutes_tens_digit <= 5; minutes_tens_digit++)
 			{
 				for (minute_digit = 0; minute_digit <= 9; minute_digit++)
 				{
 					_putchar(hours_tens_digit + '0');
+
+					if (hours_digit_turn == 2)
+					{
+						hours_digit = 3;
+					}
+
 					_putchar(hours_digit + '0');
 					_putchar(':');
 					_putchar(minutes_tens_digit + '0');
 					_putchar(minute_digit + '0');
 					_putchar('\n');
+					hours_digit_turn++;
 				}
 			}
 		}
