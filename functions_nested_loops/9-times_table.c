ac0d5801df02121564ca4d9944a4e6baa06325be
Aurylias <brycevermorel@gmail.com>
2026-06-24 10:25:09 +0200

    Trying

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,31 +7,40 @@
 
 void times_table(void)
 {
-    int table_count, table;
+	int table_count, table;
 
-    for (table = 0; table <= 9; table++)
-    {
-        for (table_count = 0; table_count <=9; table_count++)
-        {
-            int result = table * table_count;
-            if (result < 10)
-            {
-                _putchar('0' + result);
-                _putchar(',');
-                _putchar(' ');
-            }
-            else
-            {
-            _putchar('0' + (result / 10));
-            _putchar('0' + (result % 10));
-            _putchar(',');
-            _putchar(' ');
-            }
+	for (table = 0; table <= 9; table++)
+	{
+		for (table_count = 0; table_count <=9; table_count++)
+		{
+			int result = table * table_count;
 
-            if (table_count == 9)
-            {
-                _putchar('\n');
-            }
-        }
-    }
+			if (result < 10)
+			{
+				_putchar('0' + result);
+				
+				if (table_count != 9)
+				{
+					_putchar(',');
+				}
+
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+				if (result != 81)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+
+			if (table_count == 9)
+			{
+				_putchar('\n');
+			}
+		}
+	}
 }
