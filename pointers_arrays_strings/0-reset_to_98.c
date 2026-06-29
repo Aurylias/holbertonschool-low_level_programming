51d08e3d5e79aa66e0dd107bca6f7942378f3a5b
Aurylias <brycevermorel@gmail.com>
2026-06-29 16:55:24 +0200

    Fixed betty typo in 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Take an pointer to an int and updates the value it points to, to 98
+ * reset_to_98 - Take an pointer to an int and updates the value
+ * @n: A pointer
  * Return: None
  */
 
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
