9b069873860b418369c9a13366ed76f39e938be1
Aurylias <brycevermorel@gmail.com>
2026-06-30 15:43:42 +0200

    Changed 100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,23 +10,41 @@ int _atoi(char *s)
 {
 	int res = 0;
 	int i;
-	int negative = 1;
+	int plus = 0;
+	int minus = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == 43)
+		{
+			plus++;
+		}
+
 		if (s[i] == 45)
 		{
-			negative = -1;
+			minus++;
 		}
 
 		while (s[i] >= 48 && s[i] <= 57)
 		{
 			res = res * 10 + (s[i] - '0');
 			i++;
+
+			if (s[i] == 59)
+			{
+				break;
+			}
 		}
 	}
 
-	return (res * negative);
+	if (plus == minus || plus > minus)
+	{
+		return (res);
+	}
+	else
+	{
+		return (-res);
+	}
 }
 
 
