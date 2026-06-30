879d4a1c389ce0030568b698eeb361482069817c
Aurylias <brycevermorel@gmail.com>
2026-06-30 14:58:08 +0200

    Changed 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -29,7 +29,7 @@ void puts_half(char *str)
 	int len = _strlen(str) - 1;
 	int i;
 
-	if ((len / 2) % 2 != 0)
+	if (len % 2 == 0)
 	{
 		for (i = len / 2; i <= len; i++)
 		{
@@ -38,7 +38,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (i = (len + 1) / 2; i <= len; i++)
+		for (i = (len + 2) / 2; i <= len; i++)
 		{
 			_putchar(str[i]);
 		}
