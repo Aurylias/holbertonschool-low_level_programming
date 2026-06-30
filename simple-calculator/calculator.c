c78a63b7e7536272bb285bda8ef6db9cca74e653
Aurylias <brycevermorel@gmail.com>
2026-06-30 16:22:55 +0200

    Change calculator

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
 void print_menu(void);
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-int divide(int a, int b);
 
 /**
  * print_menu - Print the main menu of the calculator
@@ -19,54 +15,6 @@ void print_menu(void)
 	printf("4) Divide\n");
 	printf("0) Quit\n");
 	printf("Your choice: ");
-}
-
-/**
- * add - Add two decimal
- * @a: First value to add
- * @b: Second value to add
- * Return: a + b
- */
-
-int add(int a, int b)
-{
-	return (a + b);
-}
-
-/**
- * subtract - Sub two decimal
- * @a: First value to sub
- * @b: Second value to sub
- * Return: a - b
- */
-
-int subtract(int a, int b)
-{
-	return (a - b);
-}
-
-/**
- * multiply - Multiply two decimal
- * @a: First value to multiply
- * @b: Second value to multiply
- * Return: a * b
- */
-
-int multiply(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * divide - Divide two decimal
- * @a: First value to divide
- * @b: Second value to divide
- * Return: a / b
- */
-
-int divide(int a, int b)
-{
-	return (a / b);
 }
 
 /**
@@ -93,44 +41,16 @@ int main(void)
 				printf("Bye !");
 				return (0);
 			case 1:
-				printf("Type your first number to add: ");
-				scanf("%d", &first_num);
 
-				printf("Type your second number to add: ");
-				scanf("%d", &second_num);
-
-				printf("The result of %d + %d is %d.\n\n",
-					first_num, second_num, add(first_num, second_num));
 				break;
 			case 2:
-				printf("Type your first number to subtract: ");
-				scanf("%d", &first_num);
 
-				printf("Type your second number to subtract: ");
-				scanf("%d", &second_num);
-
-				printf("The result of %d - %d is %d.\n\n",
-					first_num, second_num, subtract(first_num, second_num));
 				break;
 			case 3:
-				printf("Type your first number to multiply: ");
-				scanf("%d", &first_num);
 
-				printf("Type your second number to multiply: ");
-				scanf("%d", &second_num);
-
-				printf("The result of %d * %d is %d.\n\n",
-					first_num, second_num, multiply(first_num, second_num));
 				break;
 			case 4:
-				printf("Type your first number to divide: ");
-				scanf("%d", &first_num);
 
-				printf("Type your second number to divide: ");
-				scanf("%d", &second_num);
-
-				printf("The result of %d / %d is %d.\n\n",
-					first_num, second_num, divide(first_num, second_num));
 				break;
 		}
 	}
