79ad0323a680d75c2aea4e54c0abd81c8628843d
Aurylias <brycevermorel@gmail.com>
2026-06-30 11:11:45 +0200

    change 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,26 @@
 #include "main.h"
 
+int _strlen(char *s);
+
+/**
+ * _strlen - Return the length of a string
+ * @s: Pointer of a string
+ * Return: The size of a string
+ */
+
+int _strlen(char *s)
+{
+	char *last = s;
+
+	while (*last++)
+	{
+
+	}
+
+	return (last - s - 1);
+}
+
+
 /**
  * print_rev - Print a string in reverse
  * @s: Pointer to a string
@@ -8,11 +29,7 @@
 
 void print_rev(char *s)
 {
-	int i;
-
-	while (*s++)
-	{
-	}
+	int i = _strlen(s);
 
 	for (; i >= 0; i--)
 	{
