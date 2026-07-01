4aaff6767ab11266103a6ab0f29e26c9735c3423
Aurylias <brycevermorel@gmail.com>
2026-07-01 14:19:00 +0200

    Add 5 for project 2

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Change all lowercase to uppercase
+ * @str: The string to change
+ * Return: Pointer to a char
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = (str[i] - 32);
+		}
+	}
+
+	return (str);
+}
