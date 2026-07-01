72140e7834ca11d57f4baa874ad1406bde1db61c
Aurylias <brycevermorel@gmail.com>
2026-07-01 15:52:21 +0200

    Changed 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -21,7 +21,7 @@ char *cap_string(char *str)
 		{
 			for (j = 0; seprarator[j] != '\0'; j++)
 			{
-				if (str[i] == seprarator[j])
+				if (str[i - 1] == seprarator[j])
 				{
 					separat = 1;
 					break;
