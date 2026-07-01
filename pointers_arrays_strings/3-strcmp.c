4be706c096019c2d55d253d26a5f02f803c3bd9f
Aurylias <brycevermorel@gmail.com>
2026-07-01 13:16:28 +0200

    Add 3 for project 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,26 +9,21 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i, j = 0, 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	while (s1[i] != '\0' && s2[j] != '\0')
 	{
-		for (j = 0; s2[j] != '\0'; j++)
+		if (s1[i] == s2[j])
 		{
-			if (s1[i] == s2[j])
-			{
-				return (0);
-			}
-			else if (s1[i] > s2[j])
-			{
-				return (1);
-			}
-			else if (s2[j] > s1[i])
-			{
-				return (-1);
-			}
+			return (0);
+		}
+		else if (s1[i] > s2[j])
+		{
+			return (1);
+		}
+		else if (s2[j] > s1[i])
+		{
+			return (-1);
 		}
 	}
-
-	return (0);
 }
