337be2765e310f673bd2745fb54d8d2653e8c8aa
Aurylias <brycevermorel@gmail.com>
2026-07-01 09:36:03 +0200

    Add 1 for project 2

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a string with \0
+ * @s: Pointer of a string
+ * Return: The size of a string
+ */
+
+int _strlen(char *s)
+{
+	char *last = s;
+
+	while (*last++)
+	{
+
+	}
+
+	return (last - s);
+}
+
+/**
+ * _strcat - Add two string together
+ * @dest: First string to add
+ * @src: Second string to add
+ * Return - Added strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int d_len = _strlen(dest);
+	int s_len = _strlen(src);
+	int i;
+
+	for (i = d_len; i <= (d_len + s_len); i++)
+	{
+		dest[i] = src[s_len - s_len];
+	}
+
+	return (dest);
+}
