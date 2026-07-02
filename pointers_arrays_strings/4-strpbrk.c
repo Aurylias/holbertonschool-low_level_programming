e81817fd572f7984f2a81823f95d1f31425ea9ba
Aurylias <brycevermorel@gmail.com>
2026-07-02 13:45:57 +0200

    Add 4 for project 3

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,29 @@
 #include "main.h"
 
 /**
- * _strpbrk -
- * @s:
- * @accept:
- * Return: 
+ * _strpbrk - Search a string for any set of bytes
+ * @s: String to search in
+ * @accept: The substring
+ * Return: Number of bytes int the initial segment of s
  */
 
-char *_strpbrk(char *s, char *accept)
-{
+char *_strpbrk(char *s, char *accept);
 
+{
+	unsigned int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				s = &s[i];
+				return (s);
+			}
+		}
+		i++;
+	}
+	return (NULL);
 }
