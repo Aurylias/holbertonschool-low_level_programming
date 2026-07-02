a46e4f1860fe3ae510798ed2fb38ce1dbbf419dd
Aurylias <brycevermorel@gmail.com>
2026-07-02 11:39:30 +0200

    Change 3 for project 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -12,11 +12,11 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0, occurence = 0;
 	int j;
 
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (str[i] == accept[j])
+			if (s[i] == accept[j])
 			{
 				occurence++;
 				break;
