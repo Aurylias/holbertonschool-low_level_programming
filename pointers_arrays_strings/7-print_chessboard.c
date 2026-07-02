57f6b2139c257310b4da81ab8a6e8c04b5f8e8fd
Aurylias <brycevermorel@gmail.com>
2026-07-02 15:40:51 +0200

    Add 7 for project 3

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Print a chess board
+ * @a: 2 dimensional array
+ * Return: None
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; i < 8; j++)
+		{
+			printf("%c", a[i][j])
+		}
+	}
+}
