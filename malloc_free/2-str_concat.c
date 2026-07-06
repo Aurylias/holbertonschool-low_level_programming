2a92c2e0786618ed31e7b1b387e0a9cfe3ca1982
Aurylias <brycevermorel@gmail.com>
2026-07-06 13:19:16 +0200

    Changed 3

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -31,14 +31,14 @@ char *str_concat(char *s1, char *s2)
 
 	new_string = malloc(sizeof(*new_string) * (len_s1 + len_s2 + 1));
 
-	for (i = 0; i <= len_s1; i++)
+	for (i = 0; i < len_s1; i++)
 	{
 		new_string[i] = s1[i];
 	}
 
 	for (i = 0; i <= len_s2; i++)
 	{
-		new_string[len_s2 + i - 1] = s2[i];
+		new_string[len_s2 + i] = s2[i];
 	}
 
 	return (new_string);
