8f9fd68cbfa481ab6b430c0a864594e58d9ef5ad
Aurylias <brycevermorel@gmail.com>
2026-07-06 16:16:02 +0200

    Change 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -25,6 +25,14 @@ int **alloc_grid(int width, int height)
 	{
 		arr[i] = malloc(sizeof(*arr) * width);
 
+		if (arr[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
+
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	}
