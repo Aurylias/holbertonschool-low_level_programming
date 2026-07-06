ddccf8fa2fbb8b5d704584613da6b7aa811beaf4
Aurylias <brycevermorel@gmail.com>
2026-07-06 11:31:17 +0200

    Add malloc_free, it's README and 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#define NULL 0
+
+/**
+* create_array - Create an array of char
+* @size: Size of the array
+* @c: Char to put int the array
+* Return: A pointer to the array
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+
+	char *str;
+	int i;
+
+	str = malloc(sizeof(*str) * size);
+
+	if (s == NULL)
+		return (1);
+
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+
+	return (str);
+}
