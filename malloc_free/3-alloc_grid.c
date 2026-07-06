7050b66662e7d2e19f2e743aaed8c4cd9a857124
Aurylias <brycevermorel@gmail.com>
2026-07-06 16:06:57 +0200

    Add 3 and update main.h

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* alloc_grid - Create a 2D array of given size
+* @width: Size of internal arrays
+* @height: Size of first array
+* Return: Pointeur to the 2D array
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(sizeof(*arr) * height);
+
+	for (i = 0; i < height; i++)
+	{
+		temp = malloc(sizeof(*arr) * width);
+		arr[i] = &temp;
+	}
+
+	return (arr);
+}
