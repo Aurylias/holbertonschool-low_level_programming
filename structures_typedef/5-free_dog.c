e96c4481666acebabf1fef7f43289144e6af1295
Aurylias <brycevermorel@gmail.com>
2026-07-07 15:44:01 +0200

    Add 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* free_dog - Free dogs
+* @d: Dog to set free
+* Return: None
+*/
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d->age);
+	free(d);
+}
