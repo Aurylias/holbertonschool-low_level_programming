73310ea2f6b84051137e0285d7b5252e678a4d1a
Aurylias <brycevermorel@gmail.com>
2026-07-07 13:49:06 +0200

    Change struct dog in main.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef dog_h
 #define dog_h
 
+/**
+ * struct dog - Structure for a dog
+ * @name: Name of the dog
+ * @owner: Name of the owner of the dog
+ * @age: Age of the dog
+ */
+
 typedef struct dog
 {
 	char *name;
-	float age;
 	char *owner;
-}
+	float age;
+};
 
 #endif
