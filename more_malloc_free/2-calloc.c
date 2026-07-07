bb7ef74a503ae8eb2538d1e9d6896352d0b097a3
Aurylias <brycevermorel@gmail.com>
2026-07-07 10:53:36 +0200

    Add 2 and update main.h

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _calloc - Allocate memory for an array
+* @nmemb: 
+* @size: 
+* Return:
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	arr = malloc(size * nmemb);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		arr[i] = 0;
+	}
+
+	return (arr);
+}
