0927be155cf40f9937a8619a14981e1f51a48898
Aurylias <brycevermorel@gmail.com>
2026-07-07 10:42:55 +0200

    Change 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,27 +16,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	for (len_s1 = 0; s1[len_s1]; len_s1++)
-	{
-
-	}
+	{}
 	for (len_s2 = 0; s2[len_s2]; len_s2++)
-	{
-
-	}
+	{}
 
 	if (n >= len_s2)
-	{
 		n = len_s2;
-	}
 
 	str = malloc(sizeof(*str) * (len_s1 + n + 1));
 
@@ -44,14 +35,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (i = 0; i < len_s1; i++)
-	{
 		str[i] = s1[i];
-	}
 
 	for (i = 0; i < n; i++)
-	{
 		str[len_s1 + i] = s2[i];
-	}
 
 	str[len_s1 + n] = '\0';
 
