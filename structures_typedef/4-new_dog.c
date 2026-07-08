886cdc646e95a9291b1dad22e3caf27e1b932876
Aurylias <brycevermorel@gmail.com>
2026-07-08 09:17:19 +0200

    Change 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,6 +16,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *n_dog;
 	int len_name;
 	int len_owner;
+	char Name[] = name;
+	char Owner[] = owner;
 
 	for (len_name = 0; name[len_name]; len_name++)
 	{}
