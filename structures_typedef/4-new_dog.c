11e302b2b5f90909d547f190b21a5ffeeb5bc3a0
Aurylias <brycevermorel@gmail.com>
2026-07-09 10:38:04 +0200

    Change 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -42,8 +42,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	n_dog->name = name;
-	n_dog->owner = owner;
+	n_dog->name = *name;
+	n_dog->owner = *owner;
 	n_dog->age = age;
 
 	return (n_dog);
