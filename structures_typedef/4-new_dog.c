6f470d59ff66fc702a6c10c8d6b898d1f6167f13
Aurylias <brycevermorel@gmail.com>
2026-07-09 10:36:47 +0200

    Change 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
-	int len_name, len_owner, i;
+	int len_name, len_owner;
 
 	for (len_name = 0; name[len_name]; len_name++)
 	{}
@@ -21,20 +21,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (len_owner = 0; owner[len_owner]; len_owner++)
 	{}
 
-	n_dog = malloc(sizeof(dog_t));
+	n_dog = (int *) malloc(sizeof(dog_t));
 	if (n_dog == NULL)
 	{
 		return (NULL);
 	}
 
-	n_dog->name = malloc(len_name);
+	n_dog->name = (int *) malloc(len_name);
 	if (n_dog->name == NULL)
 	{
 		free(n_dog);
 		return (NULL);
 	}
 
-	n_dog->owner = malloc(len_owner);
+	n_dog->owner = (int *) malloc(len_owner);
 	if (n_dog->owner == NULL)
 	{
 		free(n_dog->name);
@@ -42,8 +42,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	n_dog->name = (int *)name;
-	n_dog->owner = (int *)owner;
+	n_dog->name = name;
+	n_dog->owner = owner;
 	n_dog->age = age;
 
 	return (n_dog);
