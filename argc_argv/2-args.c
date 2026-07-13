a293206835557e9580296b957df43516724b3d6b
Aurylias <brycevermorel@gmail.com>
2026-07-13 19:29:06 +0200

    Add 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Entry point
+* @argc: Number of arguments
+* @argv: Array of arguments
+* Return: Always O (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
