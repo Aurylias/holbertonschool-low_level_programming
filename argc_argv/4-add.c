c93f27ce53d56782b9c479753df535393de1974a
Aurylias <brycevermorel@gmail.com>
2026-07-13 19:44:32 +0200

    Add 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* @argc: Number of arguments
+* @argv: Array of arguments
+* Return: Always O (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int result = 0;
+
+	if (argv[1] == NULL)
+		printf("0\n");
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i] >= '0' && argv[i] <= '9')
+		{
+			result += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d", result);
+
+	return (0);
+}
