7bbc1d08ace67840fec8cf2573ebe309b9b83511
Aurylias <brycevermorel@gmail.com>
2026-07-15 10:07:37 +0200

    Add first files for variadic project

---
+++ variadic_fonctions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - Get the sum of all arguments
+* @n: Number of argument passed
+* Return: The sum of all arguments
+*/
+
+int sum_them_all(const unsigned int n, ...);
+{
+	va_list numbers;
+	int sum = 0;
+	int i;
+
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(numbers, int);
+
+	va_end(numbers);
+
+	return (sum);
+}
---
+++ variadic_fonctions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef variadic_functions_h
+#define variadic_functions_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <srdarg.h>
+
+int _putchar(char c);
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
