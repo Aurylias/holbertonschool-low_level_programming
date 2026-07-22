d8934a9b2c20c4db99871b22e835795de198f7ad
Aurylias <brycevermorel@gmail.com>
2026-07-22 10:19:28 +0200

    CHange 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,15 +8,17 @@
 
 size_t print_list(const list_t *h)
 {
-  size_t count;
+  size_t count = 0;
 
-  for (count = 0; h != NULL; count++)
+  while (h != NULL)
   {
     if (h->str == NULL)
       printf("[0] (nil)");
     else
       printf("[%u] %s\n", h->len, h->str);
+
+    count++;
   }
-  
+
   return (count);
 }
