edb32381c5da806897e6078dae8b75b4f27b082b
Aurylias <brycevermorel@gmail.com>
2026-07-27 13:29:53 +0200

    Add 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* sum_dlistint - Get the sum of all data
+* @head: Pointer to the head
+* Return: The sum of all data
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head->next != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
