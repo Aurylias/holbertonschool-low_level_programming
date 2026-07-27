67f4be9d8a1626f1a921bf354e4373a3f07fc70f
Aurylias <brycevermorel@gmail.com>
2026-07-27 09:51:51 +0200

    Add 1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,18 @@
 #include "lists.h"
 
 /**
-* print_dlistint - Print all the int of a list
+* dlistint_len - Return the length of the list
 * @h: pointer to the first node
 * Return: Number of nodes
 */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		printf("%i\n", h->n);
-
 		count++;
-		h = h->next;
 	}
 
 	return (count);
