12296e34e5aadc522c5fcd538b2423913aa7e0a5
Aurylias <brycevermorel@gmail.com>
2026-07-27 11:26:33 +0200

    Add 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* dlistint_len - Return the length of the list
+* @h: pointer to the first node
+* Return: Number of nodes
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	unsigned int i;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define lists_h
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct dlistint_s
 {
@@ -12,5 +13,6 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif
