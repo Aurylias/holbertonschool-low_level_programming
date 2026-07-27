00bc07f19fd9918fc530db436650b3d999f46e01
Aurylias <brycevermorel@gmail.com>
2026-07-27 09:49:18 +0200

    Add 1

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -6,16 +6,13 @@
 * Return: Number of nodes
 */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		printf("%i\n", h->n);
-
 		count++;
-		h = h->next;
 	}
 
 	return (count);
