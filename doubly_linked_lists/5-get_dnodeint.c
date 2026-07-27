ace9a801a75ab175975d528f0134afd1b4484060
Aurylias <brycevermorel@gmail.com>
2026-07-27 12:14:18 +0200

    Add 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/*
+* get_dnodeint_at_index - Return the nth node of the list
+* @head: Pointer to the first node
+* @index: The index to reach
+* Return: The found node
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	while(i != 5)
+	{
+		head = head->next;
+		i++;
+	}
+
+	return (head);
+}
