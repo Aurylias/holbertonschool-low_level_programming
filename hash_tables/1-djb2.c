2a8833f70b1991bf57d22db6f6a25182cdf9d8ed
Aurylias <brycevermorel@gmail.com>
2026-07-28 13:58:13 +0200

    Fixed betty in 1

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -9,12 +9,13 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-    int c;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c;
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	
+	return (hash);
 }
