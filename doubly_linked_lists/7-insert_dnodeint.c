6333472b20822f33a378dfe540a112c0fd591777
Aurylias <brycevermorel@gmail.com>
2026-07-28 09:56:10 +0200

    Change 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -56,7 +56,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new->n = n;
 	new->next = tmp->next;
-	new->prev = tmp;
+	new->prev = tmp->prev;
 	tmp->next->prev = new;
 	tmp->next = new;
 
