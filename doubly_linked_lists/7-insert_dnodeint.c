89c0b05a773cf1603cab3164056d8f7eedd6eda5
Aurylias <brycevermorel@gmail.com>
2026-07-28 09:53:28 +0200

    Change 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+* get_dnodeint_at_index - Return the nth node of the list
+* @head: Pointer to the first node
+* @index: The index to reach
+* Return: The found node
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (i < index)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next;
+		i++;
+	}
+
+	return (head);
+}
+
+/**
 * insert_dnodeint_at_index - Insert a new node at the specified index
 * @h: Pointeur to the head of the list
 * @idx: The index to insert to
@@ -24,6 +47,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == len - 1)
 		return (add_dnodeint_end(h, n));
+
+	tmp = get_dnodeint_at_index(*h, idx);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
