d2def10779ba8e209e3fdba7dc09dc1438976786
Aurylias <brycevermorel@gmail.com>
2026-07-29 13:03:37 +0200

    Change 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,22 +10,23 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *tmp = NULL;
 	unsigned int i = 0;
-	unsigned int bool;
+	unsigned int printed = 0;
 
 	printf("{");
 	while (i < ht->size)
 	{
 		tmp = ht->array[i];
-		bool = 0;
 		if (tmp)
 		{
-			if (bool)
+			if (printed)
 				printf(", ");
 			printf("'%s': '%s'", tmp->key, tmp->value);
-			bool = 1;
+			printed = 1;
 			tmp = tmp->next;
 			while (tmp)
 			{
+				if (printed)
+					printf(", ");
 				printf("'%s': '%s'", tmp->key, tmp->value);
 				tmp = tmp->next;
 			}
