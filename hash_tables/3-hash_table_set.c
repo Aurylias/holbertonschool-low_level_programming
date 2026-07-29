7a1e95e920ca044c367f8b80bfd975cdb83189fe
Aurylias <brycevermorel@gmail.com>
2026-07-29 10:35:49 +0200

    Change 3 and .h

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,5 +17,37 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	tpm = ht->array[index];
+	tmp = ht->array[index];
+
+	if(!tmp)
+	{
+		node = (hash_node_t *)malloc(sizeof(hash_node_t));
+		if (!node)
+			return (0);
+		node->key = strdup(key);
+		node->value = strdup(value);
+		node->next = NULL;
+		ht->array[index] = node;
+	}
+	else
+	{
+		while(tmp->next != NULL)
+		{
+			if (key == tmp->key)
+			{
+				tmp->value = strdup(value);
+			}
+			else
+			{
+				node = (hash_node_t *)malloc(sizeof(hash_node_t));
+				if (!node)
+					return (0);
+				node->key = strdup(key);
+				node->value = strdup(value);
+				node->next = tmp;
+			}
+		}
+	}
+
+	return (1);
 }
