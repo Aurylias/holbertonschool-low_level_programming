ea831b20d74c65ec0018eedbe5345ed47767efdd
Aurylias <brycevermorel@gmail.com>
2026-07-29 12:24:53 +0200

    Change 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -8,24 +8,22 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *a_tmp = NULL, *l_tmp = NULL;
+	hash_node_t *a_tmp = NULL;
 	unsigned int i = 0;
 
-	a_tmp = ht->array[i];
-	while (a_tmp)
+	printf("{");
+	while (i < ht->size)
 	{
-		printf("{'%s': '%s', }", a_tmp->key, a_tmp->value);
-
-		if (!a_tmp->next)
-		{
-			l_tmp = a_tmp->next;
-			while (l_tmp)
-			{
-				printf("{'%s': '%s', }", l_tmp->key, l_tmp->value);
-				l_tmp = l_tmp->next;
-			}
-		}
-		i++;
 		a_tmp = ht->array[i];
+		printf("'%s': '%s', ", a_tmp->key, a_tmp->value);
+		a_tmp = a_tmp->next;
+		while (a_tmp)
+		{
+			printf("'%s': '%s', ", a_tmp->key, a_tmp->value);
+			a_tmp = a_tmp->next;
+		}
+
+		i++;
 	}
+	printf("}");
 }
