2912dc02004ad2a4cf910f0fa6967fa276648741
Aurylias <brycevermorel@gmail.com>
2026-07-29 13:43:28 +0200

    Add 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_delete - Delete a whole hash table
+* @ht: The hash table to erease
+* Return: None
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp = NULL;
+	hash_node_t *tmp_tmp = NULL;
+	unsigned int i = 0;
+
+	while (i < ht->size)
+	{
+		tmp = ht->array[i];
+		if (tmp)
+		{
+			tmp_tmp = tmp;
+			free(tmp->key);
+			free(tmp->value);
+			tmp = tmp->next;
+			free(tmp_tmp);
+			while (tmp)
+			{
+				tmp_tmp = tmp;
+				free(tmp->key);
+				free(tmp->value);
+				tmp = tmp->next;
+				free(tmp_tmp);
+			}
+		}
+		i++;
+	}
+	free(ht);
+}
