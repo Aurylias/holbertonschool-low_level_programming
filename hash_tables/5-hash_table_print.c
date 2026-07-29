32883a3396e68d99bc5c1c0e305d6dfe9fc93fc5
Aurylias <brycevermorel@gmail.com>
2026-07-29 12:06:23 +0200

    Change 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -11,6 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *a_tmp = NULL, *l_tmp = NULL;
 	unsigned int i = 0;
 
+	a_tmp = ht->array[i];
 	while (a_tmp)
 	{
 		a_tmp = ht->array[i];
