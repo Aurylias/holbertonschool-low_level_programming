27a5b2e3765305f6c3e607e7788f84068f72a600
Aurylias <brycevermorel@gmail.com>
2026-07-30 11:30:39 +0200

    Add 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion - Find the square root of a number
+* @n: The number to use
+* Return: The square root of the number
+*/
+
+int _sqrt_recursion(int n)
+{
+	
+}
