e3b721c88cb2b04375f7e5f6b47e15f246adfb2f
Aurylias <brycevermorel@gmail.com>
2026-08-06 15:08:09 +0200

    Complete task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,12 +1,37 @@
 #include "main.h"
 
 /**
-* _sqrt_recursion - Find the square root of a number
+* find_root - Find square root of n
+*
 * @n: The number to use
-* Return: The square root of the number
+* @i: Test to run on this root
+*
+* Return: The natural square root
+*/
+
+int find_root(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+
+	if (i * i == n)
+		return (i);
+
+	return (find_root(n, i + 1));
+}
+
+/**
+* _sqrt_recursion - find natural square root
+*
+* @n: The number to use
+*
+* Return: The natural square root
 */
 
 int _sqrt_recursion(int n)
 {
-	
+	if (n < 0)
+		return (-1);
+
+	return (find_root(n, 0));
 }
