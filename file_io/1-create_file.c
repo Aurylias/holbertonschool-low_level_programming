335cfa7fd469481f5ea3bba73359c10c4f95f882
Aurylias <brycevermorel@gmail.com>
2026-08-07 13:05:26 +0200

    Fixed first return value

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	int file, len, char_count;
 
 	if (!text_content)
-		return (-1);
+		return (1);
 
 	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (file == -1)
